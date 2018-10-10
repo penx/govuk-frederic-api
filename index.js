ba91fd6dc48833def3949ed768d861e7abd103e3
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2018-10-10 17:01:51 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const { results } = require('@govuk-frederic/sample-data')
+
+const typeDefs = gql`
+  type Item {
+    title: String
+    count: Int
+  }
+
+  type Meta {
+    title: String
+    items: [Item]
+  }
+  type Result {
+    title: String
+    synopsis: String
+    meta: [Meta]
+  }
+
+  type ResultGroup {
+    title: String
+    results: [Result]
+  }
+
+  type Query {
+    results: [Result]
+  }
+`;
+
+// Resolvers define the technique for fetching the types in the
+// schema.  We'll retrieve books from the "books" array above.
+const resolvers = {
+  Query: {
+    results: () => results.payload,
+  },
+};
+
+// In the most basic sense, the ApolloServer can be started
+// by passing type definitions (typeDefs) and the resolvers
+// responsible for fetching the data for those types.
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// This `listen` method launches a web-server.  Existing apps
+// can utilize middleware options, which we'll discuss later.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
